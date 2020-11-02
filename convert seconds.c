448cd2394a2719aff49fce29a10ef55c5f31d9d9
JessicaEstrada <73824375+JessicaEstrada@users.noreply.github.com>
2020-11-02 18:08:25 +0800

    Add files via upload

---
+++ convert seconds.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sec, h, m, s;
+	printf("Enter time in seconds: ");// 7890
+	scanf("%d", &sec);
+
+	h = (sec/3600); // 7890/3600
+
+	m = (sec -(3600*h))/60;// 7890-3600/60
+
+	s = (sec -(3600*h)-(m*60)); // 7890-3600-60
+	printf("7890 seconds = %d hours :%.2d minutes :%.2d seconds\n",h,m,s);
+    return 0;
+}
